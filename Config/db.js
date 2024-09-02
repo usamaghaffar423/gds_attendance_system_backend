2d6f40a524d604b99477c43f3be86cac98467f49
usamaghaffar423 <ossamaghaffar888@gmail.com>
2024-09-03 01:42:49 +0500

    changed db configuration

--- Config/db.js
+++ Config/db.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`Error: ${err.message}`);
-    process.exit(1); // Exit process with failure
+    process.exit(1);
   }
 };
 
